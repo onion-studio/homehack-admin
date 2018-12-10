a24823ac37946bec50d6ad49bddfb3c9f1e53486
Hong Jeesoo <jeesoo.hong@gmail.com>
2018-12-10 18:08:02 +0900

    set directory structure and add components related Todo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
+import ScreensRoot from './screens/Root';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ScreensRoot />, document.getElementById('root'));
